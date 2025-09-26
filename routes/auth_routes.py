2299b13484a2c20dd36c170373c433035cf99090
Xx-IKERO-90-xX <ikerjrh2003@gmail.com>
2025-09-26 20:47:36 +0200

    Corregimos una cosilla

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -10,22 +10,25 @@ auth_bp = Blueprint('auth', __name__)
 # Ruta del Login
 @auth_bp.route('/login', methods=['GET', 'POST'])
 async def login():
-    if request.method == 'GET':
-        return render_template('/auth/login.jinja')
-    else:
-        username = request.form['username']
-        passwd = request.form['passwd']
-        
-        if await security.verify_login(username, passwd):
-            user = db.session.query(User).filter(User.username == username).first()
-            
-            session['id'] = user.id
-            session['username'] = user.username
-
-            return redirect(url_for('pannel.index'))
+    if 'id' not in session:
+        if request.method == 'GET':
+            return render_template('/auth/login.jinja')
         else:
-            error_msg = "Fallo en el inicio de sesión!"
-            return render_template('/auth/login.jinja', error_msg=error_msg)
+            username = request.form['username']
+            passwd = request.form['passwd']
+        
+            if await security.verify_login(username, passwd):
+                user = db.session.query(User).filter(User.username == username).first()
+            
+                session['id'] = user.id
+                session['username'] = user.username
+
+                return redirect(url_for('pannel.index'))
+            else:
+                error_msg = "Fallo en el inicio de sesión!"
+                return render_template('/auth/login.jinja', error_msg=error_msg)
+    else:
+        return redirect(url_for('index'))
 
 # Ruta para cerrar sesión
 @auth_bp.route('/logout', methods=['GET'])
